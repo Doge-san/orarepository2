e864d407ad834c61e386fe431d485a2ba7d05d1f
Doge-san <>
2018-07-31 12:01:04 +0200

    Added comment line 5

--- SQLFUND_HR.prc.sql
+++ SQLFUND_HR.prc.sql
@@ -2,5 +2,5 @@ create or replace procedure exponer (base in int, exponent in int)
 as
 resulted float; --such float, very change
 begin
-dbms_output.put_line(power(base,exponent));
+dbms_output.put_line(power(base,exponent)); -- added comment, much wow!
 end;
